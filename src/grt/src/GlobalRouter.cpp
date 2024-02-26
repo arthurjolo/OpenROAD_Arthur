bc7334c7107e4bfe1d527937c8dcd107cba7aa8f
arthur <arthurjolo@gmail.com>
2024-02-26 14:22:24 -0300

    test new cetion of fake pins
    Signed-off-by: arthur <arthurjolo@gmail.com>

--- src/grt/src/GlobalRouter.cpp
+++ src/grt/src/GlobalRouter.cpp
@@ -2442,17 +2442,23 @@ void GlobalRouter::createFakePin(Pin pin,
         pin_position.setY(original_y);
         return;
       }
+      //if there is annother pin between the created fake pin and the original pin, change
+      //the fake pin position to be the same as this other pin.
       if (pin_connection.init_y == pin_connection.final_y) {
         if ((net_pin_pos.y() == pin_conn_init_y) 
             && (net_pin_pos.x() >= pin_conn_init_x)
             && (net_pin_pos.x() <= pin_conn_final_x)) {
-          pin_position.setX(original_x);
+          pin_position.setX(net_pin_pos.x());
+          pin_connection.init_x = std::min(original_x, net_pin_pos.x());
+          pin_connection.final_x = std::max(original_x, net_pin_pos.x());
         }
       } else {
         if ((net_pin_pos.x() == pin_conn_init_x) 
             && (net_pin_pos.y() >= pin_conn_init_y)
             && (net_pin_pos.y() <= pin_conn_final_y)) {
-          pin_position.setY(original_y);
+          pin_position.setY(net_pin_pos.y());
+          pin_connection.init_y = std::min(original_y, net_pin_pos.y());
+          pin_connection.final_y = std::max(original_y, net_pin_pos.y());
         }
       }
     }
